4a565af292632c6bea8bcd259db1a136683fb963
Shivam Dubey <108024596+Shivam00002@users.noreply.github.com>
2023-07-13 23:08:33 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -12,6 +12,7 @@ server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
+
 }))
 server.use(router)
 server.listen(3000, () => {
